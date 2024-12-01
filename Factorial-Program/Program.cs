8de9d3b67fffc34afcb5ca2e1746a288ab380737
samirkhan-947 <samirrkhan947@gmail.com>
2024-12-01 23:58:27 +0530

    Factorial Program

---
+++ Factorial-Program/Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //The factorial of a non-negative integer 𝑛 n is the product of all positive integers less than or equal to n. It is denoted by 𝑛 ! n!.
+        int n, fact = 1;
+        Console.WriteLine("Enter the number:");
+        n = Convert.ToInt32(Console.ReadLine());
+
+        for(int i=1; i<=n; i++)
+        {
+            fact = fact * i;
+        }
+        Console.Write("Factorial of " + n + " is " + fact);
+        Console.ReadKey(true);
+    }
+}
--- OddEvenNo/Program.cs
+++ OddEvenNo/Program.cs
@@ -2,6 +2,9 @@
 {
     private static void Main(string[] args)
     {
+        //The program checks if the number is divisible by 2 using the modulo operator (%).
+        //If number % 2 == 0, the number is even; otherwise, it is odd.
+
         int x;
         Console.WriteLine("Enter the No");
         x = Convert.ToInt32(Console.ReadLine());
