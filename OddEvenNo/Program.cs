c97da559c925785fcd307af6a482de302c597714
samirkhan-947 <samirrkhan947@gmail.com>
2024-12-01 23:34:29 +0530

    Add project files.

---
+++ OddEvenNo/Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int x;
+        Console.WriteLine("Enter the No");
+        x = Convert.ToInt32(Console.ReadLine());
+        if(x%2 == 0)
+        {
+            Console.WriteLine("Number is Even No");
+        }
+        else
+        {
+            Console.WriteLine("Number is Odd No");
+        }
+        Console.ReadKey();
+    }
+}
